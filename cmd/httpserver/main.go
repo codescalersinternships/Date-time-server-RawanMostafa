194193a4753648b33c9e20c0e36c97403fd422e3
RawanMostafa08 <rawanmostafa401@gmail.com>
2024-09-30 01:06:39 +0300

    feat: Making the port number configurable

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -1,22 +1,54 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/codescalersinternships/Datetime-server-RawanMostafa/pkg"
 )
 
+const defaltBaseUrl = "http://localhost"
+const defaultEndpoint = "/datetime"
+const defaultPort = "8083"
+
+func getFlags() string {
+	var port string
+	flag.StringVar(&port, "port", "", "Specifies the port")
+
+	flag.Parse()
+	return port
+}
+
+func decideConfigs() string {
+
+	port := getFlags()
+
+	if port == "" {
+		envPort, found := os.LookupEnv("DATETIME_PORT")
+
+		if found {
+			port = envPort
+		} else {
+			port = defaultPort
+		}
+	}
+	return port
+
+}
 func main() {
+	port := decideConfigs()
+
 	fmt.Println("Starting our simple http server.")
 
 	http.HandleFunc("/", pkg.HttpHome)
 	http.HandleFunc("/datetime", pkg.HttpHandler)
 
-	fmt.Println("Started on port", ":8080")
+	fmt.Printf("Started on port :%s\n", port)
 
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 	if err != nil {
 		log.Fatal(err)
 	}
